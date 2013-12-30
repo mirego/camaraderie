f8f58b2e2ce714e48abbf3b9db25ba0c32c1a891
Rémi Prévost <remi@exomel.com>
2013-12-30 13:41:47 -0500

    Add an extra index to the migration

--- lib/generators/camaraderie/templates/migration.rb
+++ lib/generators/camaraderie/templates/migration.rb
@@ -10,6 +10,7 @@ class AddCamaraderie < ActiveRecord::Migration
 
     add_index "memberships", ["organization_id", "membership_type"], name: "index_memberships_on_organization_id_and_membership_type"
     add_index "memberships", ["organization_id", "user_id", "membership_type"], name: "index_memberships_on_everything", unique: true
+    add_index "memberships", ["organization_id", "user_id"], name: "index_memberships_on_organization_id_and_user_id"
     add_index "memberships", ["organization_id"], name: "index_memberships_on_organization_id"
     add_index "memberships", ["user_id"], name: "index_memberships_on_user_id"
   end
